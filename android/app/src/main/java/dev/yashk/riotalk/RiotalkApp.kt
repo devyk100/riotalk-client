66a7a8e6124c939fa50d380b09456193148dfd2f
Yash Ramesh Kumar <devyk100@gmail.com>
2025-05-06 21:47:06 +0530

    removed all other platform clients

--- android/app/src/main/java/dev/yashk/riotalk/RiotalkApp.kt
+++
@@ -1,4 +0,0 @@
-package dev.yashk.riotalk
-
-class RiotalkApp {
-}
--- desktop/src/renderer/src/pages/main-page.tsx
+++
@@ -1,7 +0,0 @@
-import { Button } from "@renderer/components/ui/button";
-
-export default function MainPage() {
-    return (<>
-        <Button>Hello World</Button>
-    </>)
-}
